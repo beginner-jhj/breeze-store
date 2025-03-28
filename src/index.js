9af3feb4414757157f8486a1e72a7d0b804e588a
jhj <hj5014221@gmail.com>
2025-03-28 16:41:44 +0900

    changed js docs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Store } from "./store.js";
  * @function createStore
  * @description A helper function to create a new Store instance with the provided initial state.
  * @param {Object} [initialState={}] - The initial state object for the store.
- * @returns {Breeze} - A new instance of the store with the given initial state.
+ * @returns {Store} - A new instance of the store with the given initial state.
  */
 
 export function createStore(initialState) {
